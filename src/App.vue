978185af1d6b9e587ced9163803772de3d56da47
Dima <gutenko.dim@yandex.ru>
2022-04-02 20:28:51 +0500

    first

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="wrapper">
+		<Header />
+		<div class="wrapper-content">
+			<div class="container">
+				<router-view />
+			</div>
+		</div>
+
+		<Footer />
+	</div>
+</template>
+
+<script>
+import Header from '@/components/Header-main'
+import Footer from '@/components/Footer-main'
+export default {
+	components: { Header, Footer }
+}
+</script>
+
+<style></style>
